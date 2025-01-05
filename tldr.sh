895853d30a29f2f068a8ff589b26a385059f85fa
AndreasFerdinand <a.kasper@froeschler.net>
2025-01-05 23:38:16 +0100

    feat: tldr.sh script prints connection details now

--- tldr.sh
+++ tldr.sh
@@ -23,8 +23,25 @@ curl https://raw.githubusercontent.com/AndreasFerdinand/CombinedArms-DockerServe
 
 docker build --progress=plain -t "andreasferdinand/cnc-ca-server:${CNC_CA_RELEASE}" .
 
+LOCAL_IP_ADDRESS=$(ip route get 1 | awk '{print $7;exit}')
+PUBLIC_IP_ADDRESS=$(curl -s ifconfig.me)
+PUBLIC_HOSTNAME=$(curl -s ipinfo.io/hostname)
+
 echo -e "\033[1;32mDocker image successfully built. Starting server...\033[0m"
-echo -e "\033[1;32mConnect to Command and Conquer: Combined Arms server at localhost:${HOSTPORT} with password: ${PASSWORD}\033[0m"
+echo ""
+echo -e "\033[1;32mConnection details:\033[0m"
+echo -e "\033[1;32m\tLocal network connection\033[0m"
+echo -e "\033[1;32m\t\tIP-Address: \033[0m${LOCAL_IP_ADDRESS}"
+echo -e "\033[1;32m\t\tCan be used to access the server from local network.\033[0m"
+echo ""
+echo -e "\033[1;32m\tPublic internet connection\033[0m"
+echo -e "\033[1;32m\t\tIP-Address: \033[0m${PUBLIC_IP_ADDRESS}"
+echo -e "\033[1;32m\t\tHostname:  \033[0m${PUBLIC_HOSTNAME}"
+echo -e "\033[1;32m\t\tCan be used to access the server from internet, if external requests are forwarded by your router.\033[0m"
+echo -e "\033[1;32m\t\tCheck subsequent messages if server is reachable.\033[0m"
+echo ""
+echo -e "\033[1;32mConnect to Command and Conquer: Combined Arms server can be accessed on port \033[0m${HOSTPORT}\033[1;32m with password: \033[0m${PASSWORD}"
+echo ""
 echo -e "\033[1;32mStop and remove the container with CTRL+C\033[0m"
 
 docker run -t --rm -p "${HOSTPORT}":1234 \
